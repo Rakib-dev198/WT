559baf92281675e9be4a3f0d29ffb4f7c05f3542
Rakib-dev198 <138598943+Rakib-dev198@users.noreply.github.com>
2026-09-02 20:29:38 +0600

    admin db

---
+++ database/admin.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `admin` (
+  `user_id` int(11) NOT NULL,
+  `password` varchar(255) NOT NULL,
+)
+
+INSERT INTO `admin` (`user_id`, `password`) VALUES
+(1, '123'),
+(2, '123'),
+(3, '123');
